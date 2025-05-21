49d813b9cfa3bc9eafe50533c49176f3ebb403f5
Britton Leo Kerin <britton.kerin@gmail.com>
2025-05-21 12:08:59 -0800

    Function to help show warn if upsteam func changed

--- src/util.sh
+++ src/util.sh
@@ -8455,3 +8455,35 @@ function ble/util/message/handler:print {
 }
 
 blehook internal_PRECMD!='ble/util/message.process precmd'
+
+function warn_if_upstream_function_changed {
+
+  # Given an Upstream Function Name, Upstream Git Commit, and
+  # Cloned-and-Modified Function Name, check for any changes in $ufn relative
+  # to the upstream version of it named ${ufn}.upstream_${ugc} and if any are
+  # found print a warning advising that $cmfn should probably be changed also
+
+  touch /tmp/da_goobers
+
+  local ufn=$1 ugc=$2 cmfn=$3
+
+  # Versioned Name Of Model Function
+  local vnomf=${ufn}.upstream_${ugc}
+
+  # Common Part Of Error Message
+  local cpoem="probably it's supposed to be a defined function for it's use in ${FUNCNAME[0]} to make sense"
+
+  # If the function we're supposed to be checking aren't both defined
+  # something didn't get updated or a function name changed upstream (or we
+  # have some unknown bug :)
+  type $ufn >/dev/null \
+    || { ble/util/print "'type $ufn' failed, $cpoem" && return 1; }
+  type $vnomf >/dev/null \
+    || { ble/util/print "'type $vnomf' failed, $cpoem" && return 1; }
+
+  # +3 to get rid of the first two lines which contain the function names
+  # (and so differ), leaving only the function bodies
+  [ "$(type $ufn | tail -n +3)" = "$(type $vnomf | tail -n +3)" ] || ble/util/print "$ufn changed upstream, $cmfn should likely change to follow it (and the version saved in $vnomf updated and the Upstream Git Commit referenced in the call to ${FUNCNAME[0]} updated)"
+}
+
+
